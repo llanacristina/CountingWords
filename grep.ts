295798c7b3cdc90e483465d4cc64c71a675f549a
Alana Cristina <allanacristina124@gmail.com>
2024-03-31 14:17:57 -0300

    Atualizando grep

--- grep.ts
+++ grep.ts
@@ -4,13 +4,16 @@ import * as path from 'path';
 export class Grep {
   public searchPatternInFiles(pattern: string, files: string[]): void {
     const grepResults: string[] = [];
-
-    const regex = new RegExp(pattern); // Convertendo a string para um objeto RegExp
+    const regex = new RegExp(pattern);
 
     for (const file of files) {
+      if (!fs.existsSync(file)) {
+        continue; // Ignora o arquivo se ele não existir
+      }
+
       const lines = fs.readFileSync(file, 'utf-8').split('\n');
       lines.forEach((line, index) => {
-        if (regex.test(line)) { // Usando a expressão regular
+        if (regex.test(line)) {
           const filename = path.basename(file);
           const directory = path.dirname(file);
           const resultLine = `${directory}/${filename}: ${line}`;
@@ -23,8 +26,6 @@ export class Grep {
       const grepResultFilePath = path.join(__dirname, 'result', 'grep_result.txt');
       fs.writeFileSync(grepResultFilePath, grepResults.join('\n'));
       console.log(`Resultados de grep salvos em ${grepResultFilePath}`);
-    } else {
-      console.log('Nenhum resultado encontrado.');
     }
   }
 }
@@ -32,22 +33,20 @@ export class Grep {
 // Obtenha os argumentos da linha de comando
 const args = process.argv.slice(2);
 
-let isRegex = false;
 let pattern: string | undefined;
 let files: string[] = [];
 
 // Verifica se a opção -e está presente
 const eIndex = args.indexOf('-e');
 if (eIndex !== -1) {
-  isRegex = true;
   pattern = args[eIndex + 1];
-  files = args.slice(eIndex + 2);
+  files = args.slice(0, eIndex).concat(args.slice(eIndex + 2));
 } else {
   pattern = args[0];
   files = args.slice(1);
 }
 if (!pattern) {
-  console.error('Por favor, forneça uma expressão regular.');
+  console.error('Por favor, forneça um padrão de pesquisa.');
   process.exit(1);
 }
 
